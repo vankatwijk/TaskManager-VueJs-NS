b9a0fbec9404f1f4aef7d94b8cef035f8a37eb39
Hendrikus van Katwijk <hendrikus@resttech.io>
2019-11-21 16:26:57 +0100

    intermidate step(computer change)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <Todos />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todos
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+        <h3>Todos</h3>
+  </div>
+</template>
+
+<script>
+import {mapGetters }
+export default {
+    name:"Todos"
+}
+</script>
+
+<style>
+
+</style>
