30a400b953dd94cae735f36c51d2ddf6f98686df
Hendrikus van Katwijk <hendrikus.hpvk@gmail.com>
2020-07-19 18:26:26 +0200

    updated layout for task screen

--- Todo/app/components/App.vue
+++ Todo/app/components/App.vue
@@ -1,27 +1,49 @@
 <template>
     <Page class="page" actionBarHidden="true">
 
-        <GridLayout columns="*" rows="200, *, auto"
+        <GridLayout columns="*" rows="auto, *, auto"
             backgroundColor="lightgray">
 
-            <Label text="Task" class="title" row="0" backgroundColor="green" />
-            <Label text="Label 5" row="1" backgroundColor="orange" />
-            <StackLayout orientation="horizontal" row="2" backgroundColor="purple">
-                <Label text="Label 1" width="*" height="50"
-                    backgroundColor="red" />
-                <Label text="Label 2" width="*" height="50"
-                    backgroundColor="green" />
-                <Label text="Label 3" width="*" height="50"
-                    backgroundColor="blue" />
-                <Label text="Label 4" width="*" height="50"
-                    backgroundColor="yellow" />
+            <Label text="My Task" class="title" row="0"
+                backgroundColor="white" />
+            <ScrollView orientation="vertical" row="1"
+                backgroundColor="orange">
+
+
+                <StackLayout orientation="vertical"
+                    backgroundColor="lightgray">
+                    <Label v-for="x in 50" :text="`Label ${x}`"
+                        class="listItem" />
+                </StackLayout>
+
+            </ScrollView>
+            <StackLayout orientation="horizontal">
+
             </StackLayout>
+
+            <GridLayout columns="*,*,*,*" row="2" backgroundColor="white">
+                <Label text="p1" width="*" height="50" col="0"
+                    backgroundColor="red" />
+                <Label text="p2" width="*" height="50" col="1"
+                    backgroundColor="green" />
+                <Label text="p3" width="*" height="50" col="2"
+                    backgroundColor="blue" />
+                <Button text="+" class="addButton" @tap="onAddButtonTap"
+                    col="3" />
+            </GridLayout>
+
         </GridLayout>
     </Page>
 </template>
 
 <script>
     export default {
+        methods: {
+            onButtonTap() {
+                console.log("Button was pressed");
+            }
+        },
+
         data() {
             return {};
         }
@@ -38,8 +60,25 @@
     .description-label {
         margin-bottom: 15;
     }
+
     .title {
-        padding:50;
-        font-size:50;
+        padding: 50;
+        font-size: 50;
+    }
+
+    .addButton {
+        height: 70;
+        width: 70;
+        margin: 10;
+        font-size: 50;
+        background-color: red;
+        align: right;
+    }
+
+    .listItem {
+        padding: 20;
+        padding-left: 50;
+        font-size: 20;
+        background-color: white;
     }
 </style>
