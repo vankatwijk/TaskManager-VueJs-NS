d467ccc4afa6acedadda09717b64cf611c21306f
Hendrikus van Katwijk <hendrikus.hpvk@gmail.com>
2020-07-19 14:53:40 +0200

    init ns project with vuex

---
+++ README.md
@@ -0,0 +1,30 @@
+# Project Title
+
+Task Manager
+
+# Project Description
+
+Todo app created in nativescript - vue, the intension is to use this project a a starting point to create a daily tracker
+
+## Getting Started
+
+The instructions for installing this project on your local machine with pre-requirements can be found in the directories Todo(mobile apps).
+
+
+## Built With
+* [vue.js](https://vuejs.org/) - Javascript frontend framework
+* [vue router](https://router.vuejs.org/) - routing between vue components
+* [vuex](https://vuex.vuejs.org) - Used to manage the state between components
+* [nativescript](https://nativescript.org/) - Used to develop IOS / android apps
+
+## Authors
+
+* **Hendrikus van Katwijk** - [Github](https://github.com/vankatwijk) - [Personal website](https://hpvk.com)
+
+## Based on
+
+* **Cuberto** - [Task Manager design - dribbble](https://dribbble.com/shots/4841953-Redesign-of-Task-Manager)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ Todo/app/components/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!" android:flat="true"/>
+        <TabView android:tabBackgroundColor="#53ba82"
+                 android:tabTextColor="#c4ffdf"
+                 android:selectedTabTextColor="#ffffff"
+                 androidSelectedTabHighlightColor="#ffffff">
+            <TabViewItem title="Tab 1">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" :text="msg" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="Tab 2">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" text="Tab 2 Content" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="Tab 3">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" text="Tab 3 Content" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+        </TabView>
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
